ab0fc51cb4eba4e4084cddb13caebf81fb39cd87
Annabelx61 <MehariAA@hendrix.edu>
2024-10-05 00:45:02 -0500

    Done with Step 7

--- firebase_demo/lib/home_page.dart
+++ firebase_demo/lib/home_page.dart
@@ -53,6 +53,7 @@ class HomePage extends StatelessWidget {
                   GuestBook(
                     addMessage: (message) =>
                         appState.addMessageToGuestBook(message),
+                    messages: appState.guestBookMessages,
                   ),
                 ],
               ],
